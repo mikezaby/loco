9df1b4b34c78c8abacdca0f5a2573f5efc864703
mikezaby <mikezaby@gmail.com>
2014-02-15 15:29:41 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loco::Application.config.secret_key_base = '674074e7ee4617711e0a87820a9e49ab95bc9eb2eea51409bd7583874a7cc94c60f4a8dfdf3df7449d00ca4530c45d80fe59a9b1d3cd410b13ef95a27fc58297'
