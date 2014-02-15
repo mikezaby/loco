06707408b5d416574a085935a2cfe8c4e6c7252f
mikezaby <mikezaby@gmail.com>
2014-02-15 16:16:02 +0200

    Add Board model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140215135656) do
+ActiveRecord::Schema.define(version: 20140215141448) do
+
+  create_table "boards", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
