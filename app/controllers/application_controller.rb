8803507f8c0983b16c9b3741000c2fcbda6714b4
mikezaby <mikezaby@gmail.com>
2014-03-02 20:36:00 +0200

    Add api_response to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def api_response(resource, options = {}, &block)
+    status = options.fetch(:status, 200)
+    error_status = options.fetch(:error_status, 422)
+
+    block.call if block_given?
+
+    if status == 204
+      render nothing: true, status: status
+    else
+      render json: resource, status: status
+    end
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotDestroyed
+    render json: resource, status: error_status
+  end
 end
